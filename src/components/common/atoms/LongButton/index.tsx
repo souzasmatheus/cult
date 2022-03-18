cc0bdeb5b879058b24903ba6d570c31cc51663b9
Matheus Souza <t.matheus.souza@self.inc>
2022-03-17 23:08:33 -0300

    chore: move components into common folder

---
+++ src/components/common/atoms/LongButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StyledButton } from './styled';
+
+type Props = {};
+
+const LongButton = (props: Props) => {
+  return <StyledButton>LongButton</StyledButton>;
+};
+
+export default LongButton;
