04d1fcb1957d2233247aaf3aee2c7d3d2dd40772
AllTwos <ggmw360@gmail.com>
2022-03-27 11:46:19 -0400

    searchbar add onsubmit func

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './index.css';
 import { ThemeProvider } from 'styled-components';
 
 import { LongButton } from 'src/components/common/atoms/';
-import { SearchBar } from 'src/components/common/molecules';
+import { SearchBar } from 'src/components/common/molecules/';
 import theme from 'src/config/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SearchBar />
+      <SearchBar onSubmit={() => console.log('clicked')} />
       <LongButton onClick={() => null}>Example</LongButton>
     </ThemeProvider>
   );
