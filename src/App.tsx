7dc699efe54d81e0970accc65aa92fc27b14a28b
AllTwos <ggmw360@gmail.com>
2022-03-30 12:45:00 -0400

    add action button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from 'styled-components';
 import { LongButton, ActionButton } from 'src/components/common/atoms/';
 import { SearchBar } from 'src/components/common/molecules/';
 import { FaHeart } from 'react-icons/fa';
+import { FiMessageSquare } from 'react-icons/fi';
+import { FiRefreshCcw } from 'react-icons/fi';
 import theme from 'src/config/theme';
 
 function App() {
@@ -17,9 +19,18 @@ function App() {
       <LongButton onClick={() => null}>Example</LongButton>
       <ActionButton
         onPress={() => console.log('clicked ActionButton')}
-        icon={<FaHeart />}
+        icon={<FaHeart id="kid" />}
         counter="8"
-        iconProp="FaHeart"
+      ></ActionButton>
+      <ActionButton
+        onPress={() => console.log('clicked ActionButton')}
+        icon={<FiMessageSquare id="kid" />}
+        counter="20"
+      ></ActionButton>
+      <ActionButton
+        onPress={() => console.log('clicked ActionButton')}
+        icon={<FiRefreshCcw id="kid" />}
+        counter=""
       ></ActionButton>
     </ThemeProvider>
   );
