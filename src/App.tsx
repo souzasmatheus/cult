da23d18c27e6addf5f522b8d9f2704cb7af2fd02
AllTwos <ggmw360@gmail.com>
2022-04-03 11:20:08 -0400

    remove components from App not needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import './index.css';
 
-import { FiMessageSquare } from 'react-icons/fi';
 import { ThemeProvider } from 'styled-components';
 
-import { LongButton, ActionButton } from 'src/components/common/atoms/';
-import { SearchBar } from 'src/components/common/molecules/';
 import theme from 'src/config/theme';
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <SearchBar
-        placeholderText="Search"
-        onSubmit={() => console.log('submitted')}
-      />
-      <LongButton onClick={() => null}>Example</LongButton>
-      <ActionButton
-        onPress={() => console.log('clicked ActionButton')}
-        icon={FiMessageSquare}
-        counter="8"
-      />
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 
 export default App;
