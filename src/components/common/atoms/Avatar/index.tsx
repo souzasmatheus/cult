6a750e6d03e601655862c803decc6a75b3619eb4
AllTwos <ggmw360@gmail.com>
2022-04-21 14:30:22 -0400

    add className prop

--- src/components/common/atoms/Avatar/index.tsx
+++ src/components/common/atoms/Avatar/index.tsx
@@ -3,14 +3,11 @@ import { AvatarImg } from './styled';
 type Props = {
   handleClick: () => void;
   pic: string;
+  className?: string;
 };
 
-const Avatar = ({ handleClick, pic }: Props) => {
-  return (
-    <>
-      <AvatarImg src={pic} onClick={handleClick} />
-    </>
-  );
+const Avatar = ({ handleClick, pic, className }: Props) => {
+  return <AvatarImg src={pic} onClick={handleClick} className={className} />;
 };
 
 export default Avatar;
